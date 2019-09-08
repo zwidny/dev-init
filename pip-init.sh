dd7f1426828201aff2cbdf65f43fd955dc39d88f
zwidny <649038269@qq.com>
2019-09-08 16:10:26 +0800

    init

---
+++ pip-init.sh
@@ -0,0 +1,56 @@
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+echo "Inspect your OS -> $OS"
+
+case $OS in
+Ubuntu)
+    mkdir -p $HOME/.config/pip
+    cat <<EOT >> $HOME/.config/pip/pip.conf
+[global]
+timeout = 60
+index-url = https://pypi.doubanio.com/simple
+EOT
+    ;;
+Debian)
+    mkdir -p $HOME/.config/pip/pip.conf
+    cat <<EOT >> $HOME/.config/pip/pip.conf
+[global]
+timeout = 60
+index-url = https://pypi.doubanio.com/simple
+EOT
+    ;;
+*)
+    BITS=?
+    ;;
+esac
+echo "Generate File $HOME/.config/pip/pip.conf \t Detail as below:"
+echo "============"
+cat $HOME/.config/pip/pip.conf
+echo "============"
